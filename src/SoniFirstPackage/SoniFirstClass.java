d88d5081e63068d8c81bfd320ff81e5ab6fb55b9
priyankapiya23 <priyankakumarisumanpk@gmail.com>
2022-01-12 18:24:08 +0530

    my first commit

---
+++ src/SoniFirstPackage/SoniFirstClass.java
@@ -0,0 +1,9 @@
+package SoniFirstPackage;
+
+public class SoniFirstClass {
+	public static void main(String[] args)
+	{
+		System.out.println("i am very happy because i created my first java program");
+		}
+
+}
